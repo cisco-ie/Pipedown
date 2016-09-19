c1696284fa244c62e63b8bb83cb60fdbfe8cad3d
kkumara3 <kkumara3@cisco.com>
2016-09-19 18:21:38 +0000

    Removed Multi-threading, fixed syntax and flow for trigger monitoring

--- daemon.py
+++ daemon.py
@@ -2,43 +2,50 @@ import multiprocessing
 import time
 import sys
 import logging
+import os
+import ConfigParser
 from Monitor.link import Link
-class monitoring():
-    def monitor(section):
-        '''
-        add a confiuration file details here
-        '''
-        location = os.path.dirname(os.path.realpath(__file__))
-        config = ConfigParser.ConfigParser()
-        try:
-            config.read('monitor.config')
-            destination = config.get(section, 'destination')
-            source = config.get(section, 'source')
-            protocol = config.get(section, 'protocol')
-            grpc_server = config.get(section, 'grpc_server')
-            grpc_port = config.get(section, 'grpc_port')
-            grpc_user = config.get(section, 'grpc_user')
-            grpc_pass = config.get(section, 'grpc_pass')
-            flush_as = config.get(section, 'flush_as')
-        while True:
-            linkstate = linkstate(destination, source, protocol)
-            if result.iperf == True:
-                time.sleep(1)
-                flush(grpc_server, grpc_port, grpc_user, grpc_pass, flush_as)
+def monitor(section):
+    '''
+    add a confiuration file details here
+    '''
+    location = os.path.dirname(os.path.realpath(__file__))
+    config = ConfigParser.ConfigParser()
+    try:
+        config.read('monitor.config')
+        destination = config.get(section, 'destination')
+        source = config.get(section, 'source')
+        protocol = config.get(section, 'protocol')
+        grpc_server = config.get(section, 'grpc_server')
+        grpc_port = config.get(section, 'grpc_port')
+        grpc_user = config.get(section, 'grpc_user')
+        grpc_pass = config.get(section, 'grpc_pass')
+        flush_as = config.get(section, 'flush_as')
+    except (ConfigParser.Error, ValueError), e:
+	print e
+        sys.exit(1)
+    while True:
+        result = linkstate(destination, source, protocol)
+        print result
+        if result == True:
+            time.sleep(1)
+            flush(grpc_server, grpc_port, grpc_user, grpc_pass, flush_as)
 
-    def linkstate(destination, source):
-        logging.basicConfig(filename='example.log',level=logging.DEBUG)
-        link = Link(destination, source, protocol)
-        result = link.health(BGP)
-        logging.info(result)
-        return result.iperf
+def linkstate(destination, source, protocol):
+    logging.basicConfig(filename='example.log',level=logging.DEBUG)
+    link = Link(destination, source)
+    result = link.health(protocol)
+    logging.info(result)
+    return result
 
-    def flush(grpc_server, grpc_port, grpc_user, grpc_pass, flush_as):
-        #calling Quan script
-        return
+def flush(grpc_server, grpc_port, grpc_user, grpc_pass, flush_as):
+    #calling Quan script
+    print "Trigger flush"
+    return
 
 if __name__ == '__main__':
-    multiprocessing.log_to_stderr(logging.DEBUG)
-    d = multiprocessing.Process(name='myApp', target=monitor, args=(section))
-    d.daemon = True
-    d.start()
+#    multiprocessing.log_to_stderr(logging.DEBUG)
+#    d = multiprocessing.Process(name='myApp', target=monitor, args=('router1',))
+#    d.daemon = True
+#    d.start()
+     monitor('router1')
