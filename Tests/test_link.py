ba4af5647d721026c615f85cb3a8ba889fc3280c
Lisa Roach <lisroach@cisco.com>
2016-09-19 14:09:50 -0700

    RCA-12 Added test_health.

--- Tests/test_link.py
+++ Tests/test_link.py
@@ -1,16 +1,17 @@
 import unittest
 from mock import MagicMock
-from Monitor import link
+from Monitor.link import Link
 
 class LinkTestCase(unittest.TestCase, object):
 
     @classmethod
     def setUpClass(cls):
-        cls.test_link = link.Link('10.1.1.1', '10.1.1.2')
+        cls.grpc_client = MagicMock()
+        cls.test_link = Link('10.1.1.1', '10.1.1.2', cls.grpc_client)
 
     def test_link_object(self):
-        lnk = link.Link('10.1.1.1', '10.1.1.2', 10, 20, 5, 5)
-        self.assertIsInstance(lnk, link.Link)
+        lnk = Link('10.1.1.1', '10.1.1.2', self.grpc_client, 10, 20, 5, 5)
+        self.assertIsInstance(lnk, Link)
         self.assertEqual(lnk.bw_thres, 10)
         self.assertEqual(lnk.jitter_thres, 20)
         self.assertEqual(lnk.pkt_loss, 5)
@@ -18,7 +19,24 @@ class LinkTestCase(unittest.TestCase, object):
 
     def test_health(self):
         protocol = 'ISIS'
-        self.test_link.check_routing = MagicMock()
-        self.test_link.run_iperf = MagicMock()
+        self.test_link.check_routing = MagicMock(return_value=True)
+        self.test_link.run_iperf = MagicMock(return_value=True)
+        result = self.test_link.health(protocol)
+        self.assertTrue(result)
 
+        self.test_link.check_routing = MagicMock(return_value=False)
+        self.test_link.run_iperf = MagicMock(return_value=True)
+        result = self.test_link.health(protocol)
+        self.assertFalse(result)
+        self.test_link.run_iperf.assert_not_called()
+
+        self.test_link.check_routing = MagicMock(return_value=True)
+        self.test_link.run_iperf = MagicMock(return_value=False)
+        result = self.test_link.health(protocol)
+        self.assertFalse(result)
+
+        self.test_link.check_routing = MagicMock(return_value=False)
+        self.test_link.run_iperf = MagicMock(return_value=False)
+        result = self.test_link.health(protocol)
+        self.assertFalse(result)
 
