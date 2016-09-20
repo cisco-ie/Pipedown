640f9925c7bc58064ff21613d21b16c4df3246b2
Lisa Roach <lisroach@cisco.com>
2016-09-20 11:19:28 -0700

    RCA-12 Adds unit tests for check_routing.

--- Tests/test_link.py
+++ Tests/test_link.py
@@ -1,12 +1,15 @@
 import unittest
-from mock import MagicMock
+import os
+from mock import MagicMock, patch
 from Monitor.link import Link
+from Tools.grpc_cisco_python.client.cisco_grpc_client import CiscoGRPCClient
+
 
 class LinkTestCase(unittest.TestCase, object):
 
     @classmethod
     def setUpClass(cls):
-        cls.grpc_client = MagicMock()
+        cls.grpc_client = CiscoGRPCClient('10.1.1.1', 57777, 10, 'test', 'test')
         cls.test_link = Link('10.1.1.1', '10.1.1.2', cls.grpc_client)
 
     def test_link_object(self):
@@ -40,3 +43,14 @@ class LinkTestCase(unittest.TestCase, object):
         result = self.test_link.health(protocol)
         self.assertFalse(result)
 
+    @patch('Tools.grpc_cisco_python.client.cisco_grpc_client.CiscoGRPCClient.getoper')
+    def test_check_routing(self, mock_get):
+        location = os.path.dirname(os.path.realpath(__file__))
+        new_filepath = os.path.join(location, 'Examples/protocol-active.txt')
+        with open(new_filepath) as f:
+            output_good = f.read()
+        mock_get.return_value = output_good
+        result = self.test_link.check_routing('isis')
+        self.assertFalse(result)
+        result = self.test_link.check_routing('bgp')
+        self.assertTrue(result)
