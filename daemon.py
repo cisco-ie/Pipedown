f7edb5cabaf77d95c9e6259e9e6640908eee2132
kkumara3 <kkumara3@cisco.com>
2016-09-28 22:00:35 -0700

    Combined functions into one single function;

--- daemon.py
+++ daemon.py
@@ -8,7 +8,7 @@ from Monitor.link import Link
 from Flush.bgp_flush import Flush_BGP
 def monitor(section):
     '''
-    add a confiuration file details here
+    Add configuration details in here
     '''
     #Read in Configuration for Daemon
     location = os.path.dirname(os.path.realpath(__file__))
@@ -18,6 +18,10 @@ def monitor(section):
         destination = config.get(section, 'destination')
         source = config.get(section, 'source')
         protocol = config.get(section, 'protocol')
+        bw_thres = config.get(section, 'bw_thres')
+        jitter_thres = config.get(section, 'jitter_thres')
+        pkt_loss = config.get(section, 'pkt_loss')
+        interval = config.get(section, 'interval')
         grpc_server = config.get(section, 'grpc_server')
         grpc_port = config.get(section, 'grpc_port')
         grpc_user = config.get(section, 'grpc_user')
@@ -30,29 +34,31 @@ def monitor(section):
 
     #Set up gRPC client
     client = CiscoGRPCClient(grpc_server, int(grpc_port), 10, grpc_user, grpc_pass)
-    logging.basicConfig(filename='example.log',level=logging.DEBUG)
-    #Run Link Tool
+    logging.basicConfig(filename='router_connected.log',level=logging.DEBUG)
+    #Monitor Link to Data Center
     while True:
-        result = linkstate(destination, source, client, protocol)
-        logging.info(result)
-        if result == True:
-            flush(client, flush_as, drop_policy_name, 'Flush/get-neighborsq.json')
+        #Checking link to data center
+        link = Link(destination, source, client, bw_thres, jitter_thres, pkt_loss, interval)
+        result = link.health(protocol)
+        if result == False:
+            logging.info('Link is good')
+        else
+            #Flushing connection to Internet due to Data center link being faulty.
+            logging.warning('Link is down, triggering Flush')
+            bgp_config_fn = 'Flush/get-neighborsq.json'
+            try
+                ext_as = flush_as.split()
+                ext_as = map(int, ext_as)
+            else
+                logging.error('Flush AS is in the wrong format')
+            flush_bgp = Flush_BGP(client, ext_as, drop_policy_name, bgp_config_fn)
+            rm_neighbors = flush_bgp.get_bgp_neighbors()
+            #Currently rm_neighbors is a tuple in unicode, want to seperate the values into strings
+            #rm_neighbors_string = ''.join(e.encode('ascii','ignore') for e,y in rm_neighbors)
+            #rm_neighbors_string = str(rm_neighbors).strip('[]')
+            logging.info(rm_neighbors)
             sys.exit(1)
 
-def linkstate(destination, source, client, protocol):
-    link = Link(destination, source, client)
-    result = link.health(protocol)
-    return result
-
-def flush(client, ext_as, drop_policy_name, bgp_config_fn):
-    #calling Quan script
-    print "Triggering flush"
-    flush_bgp = Flush_BGP(client, [int(ext_as)], drop_policy_name, bgp_config_fn)
-    rm_neighbors = flush_bgp.get_bgp_neighbors()
-#    rm_neighbors_string = ''.join(e.encode('ascii','ignore') for e,y in rm_neighbors)
-#    rm_neighbors_string = str(rm_neighbors).strip('[]')
-    logging.info(rm_neighbors)
-    return
 
 if __name__ == '__main__':
 #    multiprocessing.log_to_stderr(logging.DEBUG)
