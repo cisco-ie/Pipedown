0e2a49791a2e3593dc44319b4603f83e4699a97c
kkumara3 <kkumara3@cisco.com>
2016-09-29 10:44:17 -0700

    changed location of logger function

--- daemon.py
+++ daemon.py
@@ -9,8 +9,22 @@ from Monitor.link import Link
 from Flush.bgp_flush import Flush_BGP
 def monitor(section):
     '''
-    Add configuration details in here
+    Daemon to monitor a link to Data Center using iPerf. If the link is faulty deamed by the thresholds set,
+    then the policy on the link to the Internet is changed to stop peering with external routers.
     '''
+    #Set up Logging, handler for both console and file. When application is finished, console will be removed
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+    file_handler = RotatingFileHandler('router_connected.log' , mode='a', maxBytes=10000, backupCount=1, encoding=None, delay=0)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
     #Read in Configuration for Daemon
     config = ConfigParser.ConfigParser()
     try:
@@ -29,24 +43,11 @@ def monitor(section):
         flush_as = config.get(section, 'flush_as')
         drop_policy_name = config.get(section, 'drop_policy_name')
     except (ConfigParser.Error, ValueError), e:
-        print e
+        logger.error('Config file error: %d'  % e)
         sys.exit(1)
 
     #Set up gRPC client
     client = CiscoGRPCClient(grpc_server, grpc_port, 10, grpc_user, grpc_pass)
-    #Set up Logging
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    console_handler.setFormatter(formatter)
-    file_handler = RotatingFileHandler('router_connected.log' , mode='a', maxBytes=10000, backupCount=1, encoding=None, delay=0)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
-    #Monitor Link to Data Center
     while True:
         #Checking link to data center
         link = Link(destination, source, client, bw_thres, jitter_thres, pkt_loss, interval)
@@ -56,8 +57,9 @@ def monitor(section):
         else:
             #Flushing connection to Internet due to Data center link being faulty.
             logger.critical('Link is down, triggering Flush')
-            bgp_config_fn = 'Flush/get-neighborsq.json'
+            bgp_config_fn = 'Flush/get-neighborsq.json' #This is currently static, as we support more types will add to config file
             try:
+                # Putting string of AS into a list
                 ext_as = flush_as.split()
                 ext_as = map(int, ext_as)
             except:
@@ -72,6 +74,7 @@ def monitor(section):
 
 
 if __name__ == '__main__':
+    #Reading config file for section headers
     config = ConfigParser.ConfigParser()
     try:
         config.read('monitor.config')
@@ -80,6 +83,7 @@ if __name__ == '__main__':
         print e
         sys.exit(1)
     multiprocessing.log_to_stderr(logging.DEBUG)
+    #Spawn process per a section header
     for section in sections:
         d = multiprocessing.Process(name=section, target=monitor, args=(section,))
         d.daemon = True
