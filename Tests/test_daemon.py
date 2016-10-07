32ce7acea9ac058f0ddf3fa22f0369e2100edac8
kkumara3 <kkumara3@cisco.com>
2016-10-07 12:10:59 -0700

    fixed first unittest

--- Tests/test_daemon.py
+++ Tests/test_daemon.py
@@ -2,30 +2,32 @@ import unittest
 import os
 from shutil import copyfile, move
 import sys
-sys.path.append("..")
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 import monitor_daemon
 
 class DaemonTestCase(unittest.TestCase):
     def setUp(self):
-        if os.path.isfile('../../monitor.config'):
+        self.location = os.path.dirname(os.path.realpath(__file__))
+        if os.path.isfile(os.path.join(self.location,'../monitor.config')):
             move(
-                '../../monitor.config',
-                '../../monitortest.config'
+                os.path.join(self.location,'../monitor.config'),
+                os.path.join(self.location,'../monitortest.config')
             )
 
     def test_grab_sections_good(self):
         copyfile(
-            'Config/monitor_good.config',
-            '../../monitor.config'
+            os.path.join(self.location,'Config/monitor_good.config'),
+            os.path.join(self.location,'../monitor.config')
         )
         sections = monitor_daemon.grab_sections()
         self.assertEqual(sections, ['BGP'])
 
     def tearDown(self):
-        if os.path.isfile('../../monitortest.config'):
+        if os.path.isfile(os.path.join(self.location,'../monitortest.config')):
             move(
-                '../../monitortest.config',
-                '../../monitor.config'
+                os.path.join(self.location,'../monitortest.config'),
+                os.path.join(self.location,'../monitor.config')
             )
 
 if __name__ == '__main__':
