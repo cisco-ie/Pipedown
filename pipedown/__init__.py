8ecf130f9ecc5adc2087f8ce9750e522a8e63059
Lisa Roach <lisroach@cisco.com>
2016-11-04 09:13:40 -0700

    Adding metadata.

--- pipedown/__init__.py
+++ pipedown/__init__.py
@@ -1,2 +1,7 @@
-__authors__ = 'Lisa Roach, Karthik Kumaravel'
+__author__ = 'Lisa Roach, Karthik Kumaravel, Quan Le'
+__copyright__ = 'Copyright 2016, Cisco Systems'
+__credits__ = ['Brandon Him']
+
+__license__ = 'Apache-2.0'
 __version__ = '1.2.0'
+__status__ = 'Development'
