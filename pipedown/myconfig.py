46f08cf25ec6f72fc9a751957b3cbcbfa06c8d94
Lisa Roach <lisroach@cisco.com>
2016-11-17 11:09:08 -0800

    RCA-39 Converts iPerf values to int. Adds __str__ and __repr__ and protocols.

--- pipedown/myconfig.py
+++ pipedown/myconfig.py
@@ -12,9 +12,43 @@ class MyConfig(object):
         for name in section_names:
             self.sections[name] = Section(name, parser)
             #Convert the protocols to a list.
-            self.sections[name].protocols = self.sections[name].protocols.split(',')
+            self.protocols.append(self.sections[name].protocols)
+
+    def __repr__(self):
+        return '{}(sections = {}, protocols = {})'.format(
+            self.__class__.__name__,
+            self.sections,
+            self.protocols
+        )
+
+    def __str__(self):
+        return 'Pipedown Configuration: Sections = {}, Protocols = {}'.format(
+            self.sections,
+            self.protocols
+            )
 
 class Section(object):
     def __init__(self, section, parser):
         self.health = False
         self.__dict__.update(parser.items(section))
+        try:
+            self.__int__('interval',
+                         'jitter_thres',
+                         'pkt_loss',
+                         'bw_thres')
+        except AttributeError:
+            pass
+
+    def __int__(self, *args):
+        """Convert multiple values to int."""
+        for arg in args:
+            self.__dict__[arg] = int(self.__dict__[arg])
+
+    def __repr__(self):
+        return '{}(section.__dict__ = {})'.format(
+            self.__class__.__name__,
+            self.__dict__
+        )
+
+    def __str__(self):
+        return 'Section Dictionary = {}'.format(self.__dict__)
