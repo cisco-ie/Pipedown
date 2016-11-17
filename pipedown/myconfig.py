b32a5c75c6b85ccbe813ed915fd4b3a4ba099ee5
Lisa Roach <lisroach@cisco.com>
2016-11-17 11:20:55 -0800

    RCA-39 Make protocols Section based.
    Each link cares about the section's protocols, not all of the
    protocols in the config. Therefore, removes protocols from MyConfig
    and converts those in Section to a list (iterable for making a Link).

--- pipedown/myconfig.py
+++ pipedown/myconfig.py
@@ -8,23 +8,18 @@ class MyConfig(object):
             raise ValueError('No config file found.')
         section_names = parser.sections()
         self.sections = {}
-        self.protocols = []
         for name in section_names:
             self.sections[name] = Section(name, parser)
-            #Convert the protocols to a list.
-            self.protocols.append([self.sections[name].protocols])
 
     def __repr__(self):
-        return '{}(sections = {}, protocols = {})'.format(
+        return '{}(sections = {})'.format(
             self.__class__.__name__,
-            self.sections,
-            self.protocols
+            self.sections
         )
 
     def __str__(self):
-        return 'Pipedown Configuration: Sections = {}, Protocols = {}'.format(
-            self.sections,
-            self.protocols
+        return 'Pipedown Configuration: Sections = {}'.format(
+            self.sections
             )
 
 class Section(object):
@@ -37,7 +32,13 @@ class Section(object):
                          'pkt_loss',
                          'bw_thres')
         except AttributeError:
+            #These are optional parameters.
             pass
+        try:
+            self.__dict__['protocols'] = list(self.__dict__['protocols'])
+        except AttributeError:
+            #This is NOT an optional parameter.
+            raise
 
     def __int__(self, *args):
         """Convert multiple values to int."""
