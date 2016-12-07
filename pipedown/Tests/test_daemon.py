f1a53f4137823b4f4de3119f7285f1b6841d64bb
Lisa Roach <lisroach@cisco.com>
2016-12-07 11:05:02 -0800

    RCA-47 Updates unittests with changes to monitor_daemon.

--- pipedown/Tests/test_daemon.py
+++ pipedown/Tests/test_daemon.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 from shutil import copyfile, move
-from mock import patch, Mock
+from mock import patch
 import monitor_daemon
 from Tools.exceptions import GRPCError
 from Tools.grpc_cisco_python.client.cisco_grpc_client import CiscoGRPCClient
@@ -77,21 +77,27 @@ class MonitorDaemonTestCase(unittest.TestCase, object):
         cls.grpc_client = CiscoGRPCClient('10.1.1.1', 57777, 10, 'test', 'test')
         cls.sec_config = config.__dict__['BGP']
 
+    # def test_monitor(self):
+    #     #Things to test:
+    #     # 1. Logger called with gRPC Error
+    #     # 2. 
+    #     pass
+
     @patch('Response.response.model_selection')
     def test_grpc_merge_errors(self, mock_grpc, mock_logger):
         mock_grpc.side_effect = GRPCError(
             err="{'cisco-grpc': {'errors':{'error': {'error-message': 'There was an error'}}}}"
         )
-        result = monitor_daemon.problem_flush(self.grpc_client, self.sec_config)
+        result = monitor_daemon.link_response(self.grpc_client, self.sec_config, True)
         self.assertFalse(result)
         self.assertTrue(mock_logger.error.called)
 
-        result = monitor_daemon.healthy_link(self.grpc_client, self.sec_config)
+        result = monitor_daemon.link_response(self.grpc_client, self.sec_config, False)
         self.assertTrue(result)
         self.assertTrue(mock_logger.error.called)
 
     @patch('monitor_daemon.health')
-    def test_grpc_get_errors(self, mock_grpc, mock_logger):
+    def test_link_check_grpc_errors(self, mock_grpc, mock_logger):
         mock_grpc.side_effect = GRPCError(
             err="{'cisco-grpc': {'errors':{'error': {'error-message': 'There was an error'}}}}"
         )
@@ -100,7 +106,7 @@ class MonitorDaemonTestCase(unittest.TestCase, object):
 
     @patch('monitor_daemon.Link')
     @patch('monitor_daemon.health')
-    def test_link_check_error(self, mock_link, mock_response, mock_logger):
+    def test_link_check_link_error(self, mock_link, mock_response, mock_logger):
         mock_link.side_effect = TypeError()
         with self.assertRaises(TypeError):
             result = monitor_daemon.link_check(self.sec_config, self.grpc_client)
@@ -109,43 +115,58 @@ class MonitorDaemonTestCase(unittest.TestCase, object):
 
     @patch('monitor_daemon.response.email_alert')
     @patch('monitor_daemon.response.text_alert')
-    def test_problem_alert_email(self, mock_text, mock_email, mock_logger):
+    def test_alert_response_email(self, mock_text, mock_email, mock_logger):
+        #Check if hostname is missing from the file
         copyfile(
             os.path.join(self.location, 'Examples/Config/email_alert.config'),
             self.config_path
         )
         config = monitor_daemon.MyConfig(self.config_path)
         sec_config = config.__dict__['BGP']
-        result = monitor_daemon.problem_alert(sec_config, 'BGP')
+        result = monitor_daemon.alert_response(sec_config, 'BGP', 'up')
         self.assertTrue(result)
         mock_text.assert_not_called()
+        mock_email.assert_called_with(
+            'pipedown@cisco.com',
+            "'BGP' link is up on 'rtr1'"
+            )
 
     @patch('monitor_daemon.response.email_alert')
     @patch('monitor_daemon.response.text_alert')
-    def test_problem_alert_text(self, mock_text, mock_email, mock_logger):
+    def test_alert_response_text(self, mock_text, mock_email, mock_logger):
         copyfile(
             os.path.join(self.location, 'Examples/Config/text_alert.config'),
             self.config_path
         )
         config = monitor_daemon.MyConfig(self.config_path)
         sec_config = config.__dict__['BGP']
-        result = monitor_daemon.problem_alert(sec_config, 'BGP')
+        result = monitor_daemon.alert_response(sec_config, 'BGP', 'up')
         self.assertTrue(result)
         mock_email.assert_not_called()
+        mock_text.assert_called_with(
+            '+14087784819',
+            "'BGP' link is up on 'rtr1'"
+        )
 
     @patch('monitor_daemon.response.email_alert')
     @patch('monitor_daemon.response.text_alert')
-    def test_problem_alert_both(self, mock_text, mock_email, mock_logger):
+    def test_alert_response_both(self, mock_text, mock_email, mock_logger):
         copyfile(
             os.path.join(self.location, 'Examples/Config/both_alert.config'),
             self.config_path
         )
         config = monitor_daemon.MyConfig(self.config_path)
         sec_config = config.__dict__['BGP']
-        result = monitor_daemon.problem_alert(sec_config, 'BGP')
+        result = monitor_daemon.alert_response(sec_config, 'BGP', 'down')
         self.assertTrue(result)
-        mock_text.assert_called_once()
-        mock_email.assert_called_once()
+        mock_text.assert_called_with(
+            '+14087784819',
+            "'BGP' link is down on 'rtr1'"
+        )
+        mock_email.assert_called_with(
+            'pipedown@cisco.com',
+            "'BGP' link is down on 'rtr1'"
+            )
 
     @classmethod
     def tearDownClass(cls):
